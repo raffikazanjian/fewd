23e0f0fb467372da5228cc5428adcecef797a709
raffikazanjian <raffik@hotmail.com>
2016-01-26 14:36:52 -0800

    light switch

--- class_13/blackout/js/index.js
+++ class_13/blackout/js/index.js
@@ -1,10 +1,10 @@
 
 $(function() {
-	
+
 	$('body').addClass('dark');
-	
+
 	var lightSwitch = function checkLights() {
-				
+
 		if ($('body').hasClass('dark') == 1) {
 			$('body').toggleClass('light');
 			$('body').removeClass('dark')
